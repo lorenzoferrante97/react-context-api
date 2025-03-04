a589e985d5226853237512ff2b5f6c1bdf6890d1
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-04 14:36:24 +0100

    add: api provider

--- src/contexts/ApiContext.jsx
+++ src/contexts/ApiContext.jsx
@@ -1,3 +1,9 @@
 import { createContext, useState, useContext } from "react";
 
 const apiContext = createContext();
+
+const apiProvider = ({ children }) => {
+  const value = {};
+
+  return <ApiContext.Provider value={value}>{children}</ApiContext.Provider>;
+};
