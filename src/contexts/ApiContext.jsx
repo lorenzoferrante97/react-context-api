43a588f0a3abede339c9f2b41d9b0efa64db0ed5
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-04 15:06:07 +0100

    add: get posts function -> fetch api

--- src/contexts/ApiContext.jsx
+++ src/contexts/ApiContext.jsx
@@ -5,6 +5,18 @@ const ApiContext = createContext();
 
 // create provider
 const ApiProvider = ({ children }) => {
+  const [postsList, setPostsList] = useState([]);
+
+  // fetch api -> index
+  const getPosts = () => {
+    fetch("http://localhost:3000/posts")
+      .then(response => response.json())
+      .then(data => setPostsList(data))
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
   const value = {};
 
   return <ApiContext.Provider value={value}>{children}</ApiContext.Provider>;
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,17 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Posts() {
-  const [postsList, setPostsList] = useState([]);
+  // const [postsList, setPostsList] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:3000/posts")
-      .then(response => response.json())
-      .then(data => setPostsList(data))
-      .catch(error => {
-        console.error(error);
-      });
-  });
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/posts")
+  //     .then(response => response.json())
+  //     .then(data => setPostsList(data))
+  //     .catch(error => {
+  //       console.error(error);
+  //     });
+  // });
 
   return (
     <main className='container-fluid bg-smoke-50 mt-10u rounded-xl'>
